dc5f21bb2113bc3d9fba30649611cf110f2ac5f9
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2025-05-16 15:37:19 +0700

    cors and tsx watch

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express from "express";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/authRoutes.js";
 
@@ -12,6 +13,12 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 // Routes
 // app.use('/api/items', itemRoutes);
 app.use("/api/auth", authRoutes);
