57deb32ba3442292a8f0961c614acd2d57afb7c5
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2025-05-20 09:27:47 +0700

    changed to using uuid

--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -1,25 +1,30 @@
 import express from "express";
 import { vnpay } from "../lib/vnpay.js";
 import { dateFormat, ProductCode, VnpLocale } from "vnpay";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
 router.post("/create-payment", async (req, res) => {
-  
   const tomorrow = new Date();
   tomorrow.setDate(tomorrow.getDate() + 1);
 
+  console.log(req.body);
+  console.log(uuidv4());
   const paymentUrl = vnpay.buildPaymentUrl({
-    vnp_Amount: 10000,//todo: hardcoded, change later
+    vnp_Amount: 10000, //todo: hardcoded, change later
     vnp_IpAddr: "13.160.92.202",
-    vnp_TxnRef: "123456",
-    vnp_OrderInfo: "Thanh toan don hang 123456",
+    vnp_TxnRef: uuidv4(),
+    vnp_OrderInfo: `Thanh toan don hang ${uuidv4()}`,
     vnp_OrderType: ProductCode.Other,
-    vnp_ReturnUrl: "http://localhost:3000/vnpay-return",
+    vnp_ReturnUrl: "http://localhost:5173/", //todo:hardcoded, change if deployed
     vnp_Locale: VnpLocale.VN, // 'vn' hoặc 'en'
     vnp_CreateDate: dateFormat(new Date()), // tùy chọn, mặc định là thời gian hiện tại
     vnp_ExpireDate: dateFormat(tomorrow), // tùy chọn
+  
   });
+
+  // res.redirect(paymentUrl)
   res.status(201).json(paymentUrl);
 });
 
