ca897ddda2e8ad76d6bd282411560e01a6bedfa1
Nguyen Dinh Duy <duyquangtri2004@gmail.com>
2025-05-30 00:30:53 +0700

    change to using url from .env

--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -1,11 +1,9 @@
 import express from "express";
-import { vnpay } from "../lib/vnpay.js";
-import { dateFormat, ProductCode, VnpLocale } from "vnpay";
-import { v4 as uuidv4 } from "uuid";
+
 import { verifyStudent, verifyToken } from "@/middlewares/authMiddleware.js";
 import TutorScheduleModel from "@/models/TutorSchedule.js";
 import PaymentTransaction from "@/models/payment/PaymentTransaction.js";
-import User from "@/models/User.js";
+
 import { createPayment } from "@/controllers/paymentController.js";
 
 const router = express.Router();
@@ -15,7 +13,7 @@ router.post("/create-payment", verifyToken, verifyStudent, createPayment);
 router.get("/vnpay/return", async (req, res) => {
   const vnpResponse = req.query;
 
-  console.log(vnpResponse);
+  const frontendUrl = process.env.FRONT_END_URL;
   const payment = await PaymentTransaction.findById(vnpResponse.vnp_TxnRef);
   const tutorSchedule = await TutorScheduleModel.findById(payment?.scheduleId);
 
@@ -27,16 +25,16 @@ router.get("/vnpay/return", async (req, res) => {
       await payment.save();
       await tutorSchedule.save();
       res.redirect(
-        `http://localhost:5173/payment-confirmation?vnp_TxnRef=${vnpResponse.vnp_TxnRef}&vnp_TransactionNo=${vnpResponse.vnp_TransactionNo}&vnp_Amount=${vnpResponse.vnp_Amount}&vnp_BankCode=${vnpResponse.vnp_BankCode}`,
+        `${frontendUrl}/payment-confirmation?vnp_TxnRef=${vnpResponse.vnp_TxnRef}&vnp_TransactionNo=${vnpResponse.vnp_TransactionNo}&vnp_Amount=${vnpResponse.vnp_Amount}&vnp_BankCode=${vnpResponse.vnp_BankCode}`,
       );
     } else {
       payment.status = "failed";
 
       await payment.save();
-      res.redirect(`http://localhost:5173/payment-failed`); //todo:hardcoded
+      res.redirect(`${frontendUrl}/payment-failed`); //todo:hardcoded
     }
   } else {
-    res.redirect(`http://localhost:5173/payment-failed`); //todo:hardcoded
+    res.redirect(`${frontendUrl}/payment-failed`); //todo:hardcoded
   }
 });
 
